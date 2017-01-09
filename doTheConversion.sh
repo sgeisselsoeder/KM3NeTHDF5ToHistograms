bab0ad66dea2a35f4b2d5057d78bbd8d26d5dcd1
Stefan Geisselsoeder <stefan.geisselsoeder@uni-erlangen.de>
2017-01-09 13:29:43 +0100

    renamed the hits to hists script again

--- doTheConversion.sh
+++ doTheConversion.sh
@@ -1,4 +1,4 @@
 python h5ToHits.py km3_v4_numuCC_100.JTE_r2356.root.h5
 python hitsToXYZHits.py km3_v4_numuCC_100.JTE_r2356.root.h5
-python hitsAndXYZHitsToHist.py km3_v4_numuCC_100.JTE_r2356.root.h5
+python hitsAllToHists.py km3_v4_numuCC_100.JTE_r2356.root.h5
 
