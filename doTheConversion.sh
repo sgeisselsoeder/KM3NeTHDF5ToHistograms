97250ea1305cd7ab1d0b11c84158f4b2c4aa4779
Stefan Geisselsoeder <stefan.geisselsoeder@uni-erlangen.de>
2017-01-09 13:37:33 +0100

    separated the two histogram creations also in the main bash script

--- doTheConversion.sh
+++ doTheConversion.sh
@@ -1,4 +1,10 @@
+# dump hdf5 files to ASCII hits
 python h5ToHits.py km3_v4_numuCC_100.JTE_r2356.root.h5
-python hitsToXYZHits.py km3_v4_numuCC_100.JTE_r2356.root.h5
-python hitsAllToHists.py km3_v4_numuCC_100.JTE_r2356.root.h5
+
+# create histograms from the hits, resolved for IDs (line, OM, and pot. PMT) and time
+python hitsIDsToHists.py km3_v4_numuCC_100.JTE_r2356.root.h5
+
+# create histograms from the hits, resolved for XYZ and time
+# python hitsToXYZHits.py km3_v4_numuCC_100.JTE_r2356.root.h5
+# python hitsXYZTToHists.py km3_v4_numuCC_100.JTE_r2356.root.h5
 
