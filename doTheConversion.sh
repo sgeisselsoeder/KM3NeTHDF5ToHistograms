4375ced52f7a08751873f6ef28ea49a2b51a75ef
Stefan Geisselsoeder <stefan.geisselsoeder@uni-erlangen.de>
2017-01-11 15:41:21 +0100

    slightly renamed the previous conversions, introduced a 4dimensional hist (xyzt) which can be stored as 4dimensional or time series of 3dimensional (xyz) hists. the latter is not finished

--- doTheConversion.sh
+++ doTheConversion.sh
@@ -2,10 +2,12 @@
 # data.h5 used to be km3_v4_numuCC_100.JTE_r2356.root.h5
 python h5ToHits.py data.h5
 
-# create histograms from the hits, resolved for IDs (line, OM, and pot. PMT) and time
-python hitsIDsToHists.py data.h5
+# extend the hits based on OM ID to XYZ coordinates
+# python hitsToXYZHits.py data.h5
+
+# create histograms from the hits resolved for IDs (line, OM, and pot. PMT) and time
+python hitsIDsTo2DHist.py data.h5
 
 # create histograms from the hits, resolved for XYZ and time
-# python hitsToXYZHits.py data.h5
-# python hitsXYZTToHists.py km3_v4_numuCC_100.JTE_r2356.root.h5
+python hitsXYZTTo2DHists.py data.h5
 
--- hitsXYZTTo4DHist.py
+++ hitsXYZTTo4DHist.py
@@ -29,6 +29,7 @@ def readNumpyArrayFromFile(filename):
 	# parse the read data to a numpy array
 	return singleStringTo2dNumpyArray(dataPlain)
 
+
 def storeHistogramAsPGM(hist, filename):
 	histFile = open(filename, 'w')
 	# write a valid header for a pgm image file
@@ -54,6 +55,52 @@ def storeHistogramAsPGM(hist, filename):
 	histFile.close()
 
 
+def store4dHistogramAsPlainFile(hist, filename):
+	histFile = open(filename, 'w')
+	# write a valid header for a pgm image file
+	# write the actual data
+	for row in hist[0]:
+		for secondRow in row:
+			for thirdRow in secondRow:
+				for entry in thirdRow:
+					# write the actual values
+					histFile.write(str(float(entry)) + " ")
+					"""
+					# or binarize the histogram 
+					# Advantage: the simulation of number of PMTs could be off. this is ignore by this
+					# disadvantage: only keeps the information: OM hit at that time or not
+					if entry >= 1: 
+						histFile.write("255 ")
+					else:
+						histFile.write("50 ")
+					"""
+	histFile.close()
+
+def store4dHistogramAsTimeSeriesOf3dHists(hist, filenameBase):
+	numberOfTimeBins = len(hist[0][0][0])
+	print numberOfTimeBins
+	for time in range(0,numberOfTimeBins):
+		histFile = open(filenameBase+str(time)+".hist", 'w')
+		# write a valid header for a pgm image file
+		# write the actual data
+		for xDim in hist[0]:
+			for yDim in xDim:
+				for zDim in yDim:
+					entry = zDim[time]
+					# write the actual value
+					histFile.write(str(float(entry)) + " ")
+					"""
+					# or binarize the histogram 
+					# Advantage: the simulation of number of PMTs could be off. this is ignore by this
+					# disadvantage: only keeps the information: OM hit at that time or not
+					if entry >= 1: 
+						histFile.write("255 ")
+					else:
+						histFile.write("50 ")
+					"""
+		histFile.close()
+
+
 
 if len(sys.argv) < 2 or str(sys.argv[1]) == "-h":
 	print "Usage: python " + str(sys.argv[0]) + " file.h5"
@@ -65,8 +112,8 @@ filenameTracks = filenameBase + "_tracks.txt"
 filenameHits = filenameBase + "_hitsXYZ.txt"
 filenameHitsTriggered = filenameBase + "_hitsTriggeredXYZ.txt"
 filenameGeometry = "km3GeoOm.txt"
-manuallySetNumberOfBinsInTime = 100
-manuallySetNumberOfBinsInSpace = 18
+manuallySetNumberOfBinsInTime = 10
+manuallySetNumberOfBinsInSpace = 2
 
 # read in the geometry
 geo = readNumpyArrayFromFile(filenameGeometry)
@@ -100,50 +147,31 @@ zeniths = np.array(tracks[:,2], np.float32)
 
 # read in all hits for all events
 hits = readNumpyArrayFromFile(filenameHits)
-print hits
+# print hits
 # assuming the format to be: event_id dom_id channel_id time
 
 # evaluate each event separately
 allEventNumbers = set(hits[:,0])	# TODO: use the set of tracks to also include events that did not produce any hits(?)
-for eventID in allEventNumbers:
-# for i in (0,2):
-#	eventID = str(i)
+# for eventID in allEventNumbers:
+for i in (0,2):
+	eventID = str(i)
 	# evaluate one event
 	
 	# filter all hits belonging to this event
 	currentHitRows = np.where(hits[:,0] == eventID)[0]
 	print "... found " + str(len(currentHitRows)) + " hits for event " + str(eventID)
-	curHits = hits[currentHitRows]
-	
-	# slice out the times of the current hits
-	times = np.array(curHits[:,4], np.float32)
-	timeOfFirstHit = np.amin(times)
-	timesRelative = times - timeOfFirstHit
-	# slice out the coordinates of the current hits
-	x = np.array(curHits[:,1], np.float32)
-	y = np.array(curHits[:,2], np.float32)
-	z = np.array(curHits[:,3], np.float32)
+	curHitsWithEventID = hits[currentHitRows]
+	# curHits = hits[currentHitRows]
+	curHits = np.array(curHitsWithEventID[:,1:5], np.float32)
+	# print curHitsWithEventID[1:5]
+	# print curHits[1:5]
 
-	# create histograms for this event
-	histXvsT = np.histogram2d(timesRelative, x, [numberBinsT, numberBinsX])
-	histYvsT = np.histogram2d(timesRelative, y, [numberBinsT, numberBinsY])
-	histZvsT = np.histogram2d(timesRelative, z, [numberBinsT, numberBinsZ])
-	histXvsY = np.histogram2d(y, x, [numberBinsY, numberBinsX])
-	histXvsZ = np.histogram2d(z, x, [numberBinsZ, numberBinsX])
-	histYvsZ = np.histogram2d(z, y, [numberBinsZ, numberBinsY])
-	# histIDvsT = np.histogram2d(times, ids, [numberBinsT, numberBinsID], [[consideredStart, consideredEnd],])
+	histXYZT = np.histogramdd(curHits, [numberBinsX, numberBinsY, numberBinsZ, numberBinsT])
+	print histXYZT[0]
 
-	# store the histograms to files
-	storeHistogramAsPGM(histXvsT, "results/xt/hist_"+filenameTracks+"_event"+str(eventID)+"_TvsX.pgm") 
-	storeHistogramAsPGM(histYvsT, "results/yt/hist_"+filenameTracks+"_event"+str(eventID)+"_TvsY.pgm") 
-	storeHistogramAsPGM(histZvsT, "results/zt/hist_"+filenameTracks+"_event"+str(eventID)+"_TvsZ.pgm") 
-	storeHistogramAsPGM(histXvsY, "results/xy/hist_"+filenameTracks+"_event"+str(eventID)+"_XvsY.pgm") 
-	storeHistogramAsPGM(histXvsZ, "results/xz/hist_"+filenameTracks+"_event"+str(eventID)+"_XvsZ.pgm") 
-	storeHistogramAsPGM(histYvsZ, "results/yz/hist_"+filenameTracks+"_event"+str(eventID)+"_YvsZ.pgm") 
-
-
-
-
+	# store the 4 dimensional histogram to file
+	store4dHistogramAsPlainFile( histXYZT, "results/xyzt/hist_"+filenameTracks+"_event"+str(eventID)+"_XYZT.hist")
+	store4dHistogramAsTimeSeriesOf3dHists( histXYZT, "results/xyzTimeSeries/hist_"+filenameTracks+"_event"+str(eventID)+"_XYZ")
 
 	"""
 	# Remove outliers and only consider hits close to the mean time for this event
