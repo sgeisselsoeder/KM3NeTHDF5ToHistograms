ae1f27a86f3a32224be60b510ce130e2d6543ed5
Stefan Geisselsoeder <sgeisselsoeder@km3net.de>
2017-01-16 16:41:10 +0100

    uncommented the generation of the hit lists in the doTheConversion script

--- doTheConversion.sh
+++ doTheConversion.sh
@@ -1,10 +1,10 @@
 # data.h5 used to be km3_v4_numuCC_100.JTE_r2356.root.h5
 
 # dump hdf5 files to ASCII hits
-# python h5ToHits.py data.h5
+python h5ToHits.py data.h5
 
 # extend the hits based on OM ID to XYZ coordinates
-# python hitsToXYZHits.py data.h5
+python hitsToXYZHits.py data.h5
 
 # create histograms from the hits resolved for IDs (line, OM, and pot. PMT) and time
 python hitsIDsTo2DHist.py data.h5
