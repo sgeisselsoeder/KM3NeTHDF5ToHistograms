739b7367de7f6bc6a17f3235afc94630dc6ac60d
Stefan Geißelsöder <stefan.geisselsoeder@fau.de>
2017-01-17 18:36:34 +0100

    added warning before i'll have forgotten

--- results/4dTo2d/collect.sh
+++ results/4dTo2d/collect.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 for i in {xt,yt,zt,xy,xz,yz} ; do echo $i && cat $i/*.hist >> ../${i}.csv ; done
+echo "I did my job, but remember to remove the last whitespace from every line in every file. Automate that! Then delete this message!"
