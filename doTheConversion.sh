a742056b4bb383ca207d55df4c6df0910efe6895
Stefan Geisselsoeder <sgeisselsoeder@km3net.de>
2017-01-31 16:57:19 +0100

    doConversion works on all files again ...

--- doTheConversion.sh
+++ doTheConversion.sh
@@ -5,8 +5,8 @@
 #	# time python hitsToHistograms.py $f
 #done
 
-# for f in ../data/km3netHDF5Files/*.h5; do 
-for f in ../data/km3netHDF5Files/km3_v4_anueCC_100.JTE_r2356.root.h5; do 
+# for f in ../data/km3netHDF5Files/km3_v4_anueCC_100.JTE_r2356.root.h5; do 
+for f in ../data/km3netHDF5Files/*.h5; do 
 	time python hitsToXYZHits.py $f
 done
 
