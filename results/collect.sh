a15384e038d212229dc755e6e910cfbbacff3d85
Stefan Geißelsöder <stefan.geisselsoeder@fau.de>
2017-02-02 08:17:05 +0100

    added omID files to the default workflow. might have to drop them again, they're huge

--- results/collect.sh
+++ results/collect.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-for i in {xt,yt,zt,xy,xz,yz} ; do echo $i && cat 4dTo2d/${i}/*.csv >> ${i}wWS.csv ; sed 's/ *$//' ${i}wWS.csv > ${i}.csv ; rm ${i}wWS.csv ; done
-# head -n 793 zt.csv > ztTrain.csv
-# tail -n 793 zt.csv > ztTest.csv
+for i in {xt,yt,zt,xy,xz,yz} ; do echo $i && cat 4dTo2d/${i}/*.csv >> ${i}.ssv ; sed 's/ *$//' ${i}.ssv > ${i}.csv ; rm ${i}.ssv ; done
+echo omIDt && cat 2dTo2d/omIDt/*.csv >> omIDt.ssv ; sed 's/ *$//' omIDt.ssv > omIDt.csv ; rm omIDt.ssv
