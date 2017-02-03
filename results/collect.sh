a584e04107a992eb0a1f2d9f860715a5da8496bc
Stefan Geißelsöder <stefan.geisselsoeder@fau.de>
2017-02-03 22:24:02 +0100

    coarser omid histograms, less output as pgm and omidt instead of omIDt in output

--- results/collect.sh
+++ results/collect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 for i in {xt,yt,zt,xy,xz,yz} ; do echo $i && cat 4dTo2d/${i}/*.csv >> ${i}.ssv ; sed 's/ *$//' ${i}.ssv > ${i}.csv ; rm ${i}.ssv ; done
-echo omIDt && cat 2dTo2d/omIDt/*.csv >> omIDt.ssv ; sed 's/ *$//' omIDt.ssv > omIDt.csv ; rm omIDt.ssv
+echo omidt && cat 2dTo2d/omIDt/*.csv >> omidt.ssv ; sed 's/ *$//' omidt.ssv > omidt.csv ; rm omidt.ssv
