9442377b6726105c8d1444fe5689d81589f3f8ac
Stefan Geißelsöder <stefan.geisselsoeder@fau.de>
2017-02-27 23:16:38 +0100

    deleted deprecated scripts, updated the doConversion script

--- doTheConversion.sh
+++ doTheConversion.sh
@@ -2,13 +2,8 @@
 #HDFFOLDER=~/woodyhome/hdf5files
 HDFFOLDER=./hdf5files
 
-time python h5ToHistograms.py ${HDFFOLDER}/km3_v4_anueCC_12.JTE_r2356.root.h5
-
-#for f in ${HDFFOLDER}/*.h5; do 
-#	time python h5ToHits.py $f
-#done
-#for f in ${HDFFOLDER}/*.h5; do 
-#	time python hitsToHistograms.py $f
-#done
-#cd results && time ./collect.sh ; cd ..
+for f in ${HDFFOLDER}/*.h5; do 
+	time python h5ToHistograms.py $f
+done
+cd results && ./collect.sh ; cd ..
 
