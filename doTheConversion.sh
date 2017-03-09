fbebfea626120b3ead140c9d4135b19bb3b90db8
Stefan Geisselsoeder <stefangeisselsoeder-github@yahoo.de>
2017-03-09 15:15:56 +0100

    doConversion script progresses by number instead of by particle flavour and interaction (allows intermediate results to be useful)

--- doTheConversion.sh
+++ doTheConversion.sh
@@ -1,6 +1,18 @@
 HDFFOLDER=./hdf5files
 
-time python h5ToHistograms.py ${HDFFOLDER}/km3_v4_numuNC_37.JTE_r2356.root.h5
+#time python h5ToHistograms.py ${HDFFOLDER}/km3_v4_numuNC_37.JTE_r2356.root.h5
+
+for i in {1..100};
+do
+	time python h5ToHistograms.py ${HDFFOLDER}/km3_v4_anumuNC_${i}.JTE_r2356.root.h5
+	time python h5ToHistograms.py ${HDFFOLDER}/km3_v4_anumuCC_${i}.JTE_r2356.root.h5
+	time python h5ToHistograms.py ${HDFFOLDER}/km3_v4_numuNC_${i}.JTE_r2356.root.h5
+	time python h5ToHistograms.py ${HDFFOLDER}/km3_v4_numuCC_${i}.JTE_r2356.root.h5
+	time python h5ToHistograms.py ${HDFFOLDER}/km3_v4_anueNC_${i}.JTE_r2356.root.h5
+	time python h5ToHistograms.py ${HDFFOLDER}/km3_v4_anueCC_${i}.JTE_r2356.root.h5
+	time python h5ToHistograms.py ${HDFFOLDER}/km3_v4_nueNC_${i}.JTE_r2356.root.h5
+	time python h5ToHistograms.py ${HDFFOLDER}/km3_v4_nueCC_${i}.JTE_r2356.root.h5
+done
 
 #for f in ${HDFFOLDER}/*.h5; do 
 #	python h5ToHistograms.py $f
