63d89d53d17a8921b9c50270bfbc2ae3a1dc0b8c
Stefan Geisselsoeder <stefangeisselsoeder-github@yahoo.de>
2017-04-06 15:34:49 +0200

    Increases number of events per split to better match the available statistics

--- results/finish.sh
+++ results/finish.sh
@@ -2,7 +2,7 @@
 ./collect.sh
 ./collechtNumu.sh
 # split xyzt in bunches of approx 120k
-split -l 123020 --numeric-suffixes=1 --suffix-length=1 xyzt.csv xyztSplit
+split -l 123321 --numeric-suffixes=1 --suffix-length=1 xyzt.csv xyztSplit
 for f in xyztSplit* ; do shuf $f > ${f}Shuf.csv ; done
 ./zipAll.sh
 ./clearResults.sh
