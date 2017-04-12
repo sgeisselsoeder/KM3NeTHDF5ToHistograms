d13a471f0495730aca4680f886ea1b148f7e75b9
Stefan Geisselsoeder <stefangeisselsoeder-github@yahoo.de>
2017-04-12 19:19:41 +0200

    Updates the finish script to create larger bunches (due to less time steps) and to measure the durations of the steps

--- results/finish.sh
+++ results/finish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-./collect.sh
-./collechtNumu.sh
-# split xyzt in bunches of approx 120k
-split -l 123321 --numeric-suffixes=1 --suffix-length=1 xyzt.csv xyztSplit
-for f in xyztSplit* ; do shuf $f > ${f}Shuf.csv ; done
-./zipAll.sh
-./clearResults.sh
+time ./collect.sh
+time ./collechtNumu.sh
+# split xyzt in bunches
+time split -l 200000 --numeric-suffixes=1 --suffix-length=1 xyzt.csv xyztSplit
+for f in xyztSplit* ; do time shuf $f > ${f}Shuf.csv ; done
+time ./zipAll.sh
+time ./clearResults.sh
 
