cfcd0249eca0aea847db00867dc5c994408ebb07
Stefan Geißelsöder <stefangeisselsoeder-github@yahoo.de>
2017-04-13 09:58:23 +0200

    Adds a script to convert ascii csv file to more efficient binary hdf5 files

---
+++ results/convertAsciiCsvToBinaryHdf5.py
@@ -0,0 +1,80 @@
+import numpy as np
+import h5py
+import csv
+
+datatypey = np.float16
+datatypex = np.uint8
+numx = 11
+numy = 11
+numz = 18
+numt = 50
+
+
+def splitLineInXAndY(lineAsString):
+	lineAsString = lineAsString.strip('\n') # remove newline at the end of the line
+        lineSplit = lineAsString.split(',') # split the line into individual numbers
+        if lineSplit[len(lineSplit)-1] == "":
+            lineSplit = lineSplit[0:len(lineSplit)-1] # remove the last empty entry due to trailing "," in poor csv implementation 
+        line = np.array(lineSplit, datatypey)   # store as numpy array
+        numMC = int(line[0]+0.001)        # the first number in each line states how many MC related infos are present
+	# TODO: encode the actual geometry here
+        # x = line[numMC+1:].reshape(1, numx, numy, numz, numt).astype(datatypex)         # everything after the first numMC+1 entries are features
+        x = line[numMC+1:].reshape(1, len(line)-numMC-1).astype(datatypex)         # everything after the first numMC+1 entries are features
+
+        # the first numMC+1 entries (but 0 is numMC itself) are information from MC (often labels) and may not be used as input for training! 
+        y = line[1:numMC+1]
+	return (x, y)
+
+
+def readDataFromFile(filename):
+	xs = []
+	ys = []
+
+	f = open(filename)
+	for lineAsString in f:
+		x, y = splitLineInXAndY(lineAsString)
+		xs.append(x)
+		ys.append(y)
+	f.close()
+	xsnp = np.array(xs, datatypex)
+	ysnp = np.array(ys, datatypey)
+	return (xsnp, ysnp)
+
+import sys
+
+# print 'Number of arguments:', len(sys.argv), 'arguments.'
+# print 'Argument List:', str(sys.argv)
+
+filename = ""
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+if filename == "":
+    print "Please specify a csv file to convert. Usage " + sys.argv[0] + " ./path/to/test.csv"
+    exit(-1)
+
+x, y = readDataFromFile(filename)
+
+#Create HDF5 file
+f = h5py.File(filename+".h5", 'w')
+f.create_dataset('x', data=x)
+f.create_dataset('y', data=y)
+f.close()
+
+#Create HDF5 file gzip
+f = h5py.File(filename+"_gz1.h5", 'w')
+f.create_dataset('x', data=x, compression="gzip", compression_opts=1)
+f.create_dataset('y', data=y, compression="gzip", compression_opts=1)
+f.close()
+
+#Create HDF5 file gzip
+f = h5py.File(filename+"_gz9.h5", 'w')
+f.create_dataset('x', data=x, compression="gzip", compression_opts=9)
+f.create_dataset('y', data=y, compression="gzip", compression_opts=9)
+f.close()
+
+#Create HDF5 file lzf
+f = h5py.File(filename+"_lzf.h5", 'w')
+f.create_dataset('x', data=x, compression="lzf")
+f.create_dataset('y', data=y, compression="lzf")
+f.close()
+
